dc049d465b2316751f8a8b06eb9c4bc0bc641a78
Armaan Goel <armaangoel78@gmail.com>
2019-01-21 18:36:25 -0500

    gets top two bounding boxes, gets distance and offset from side

--- DeepSpace2.py
+++ DeepSpace2.py
@@ -26,43 +26,62 @@ class DeepSpace2:
         self.timer = jevois.Timer("processing timer", 100, jevois.LOG_INFO)
         
     def process(self, inframe, outframe):
+        text = ""
         raw = inframe.getCvBGR()
                 
         self.timer.start()
         
-        # convert to hsv colorspace
         hsv = cv2.cvtColor(raw,cv2.COLOR_BGR2HSV)
         
-        # filter based on hsv vales
-        min = np.array([45, 50, 180])
-        max = np.array([125, 255,255])
+        min = np.array([81,  0, 220])
+        max = np.array([110, 19, 255])
         filtered = cv2.inRange(hsv, min, max)
         
             # kernel = np.ones((5,5),np.uint8)
             # eroded = cv2.erode(filtered,kernel,iterations = 2)
             # dilated = cv2.dilate(eroded,kernel,iterations = 4)
 
-        # detect edges        
         edged = cv2.Canny(filtered, 30, 200)
-        
-        # detect contours
+
+             
         cnts, hierarchy = cv2.findContours(edged.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
         if (cnts is not None):
-            # find 10 contours of biggest area 
             cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-        
-            text = "No object detected"
+
+            box_one = cv2.boundingRect(cnts[0])
+            box_two = None
+
+            for c in cnts:
+                x,y,w,h = cv2.boundingRect(c)
+                if (np.absolute(box_one[0] - x) > box_one[2]):
+                    box_two = (x,y,w,h)
+                    break
+
+            x0,y0,w0,h0 = box_one
+            x1,y1,w1,h1 = box_two
+
+            cv2.rectangle(raw,(x0,y0),(x0+w0,y0+h0),(0,255,0),2)
+            cv2.rectangle(raw,(x1,y1),(x1+w1,y1+h1),(0,255,0),2)
+
+            dist = np.absolute(x0-x1)
+            mid = np.minimum(x0,x1) + dist/2
+
+            text = "dist: " + str(dist) + " mid: " + str(mid)
             
-            if (len(cnts) > 0):
-                # draw bounding box around object
-                x,y,w,h = cv2.boundingRect(cnts[0])
-                cv2.rectangle(raw,(x,y),(x+w,y+h),(0,255,0),2)
-                text = "position: (" + str(x) + "," + str(y) + "), height: " + str(h) + ", width: " + str(w)
+            # if (len(cnts) > 0):
+            #     # draw bounding box around object
+            #     x,y,w,h = cv2.boundingRect(cnts[0])
+            #     cv2.rectangle(raw,(x,y),(x+w,y+h),(0,255,0),2)
+            #     text = "position: (" + str(x) + "," + str(y) + "), height: " + str(h) + ", width: " + str(w)
+
+            #     x,y,w,h = cv2.boundingRect(cnts[1])
+            #     cv2.rectangle(raw,(x,y),(x+w,y+h),(0,255,0),2)
+            #     text = "position: (" + str(x) + "," + str(y) + "), height: " + str(h) + ", width: " + str(w)
         
         outimg = raw
         
-        cv2.putText(outimg, "JeVois DeepSpace2", (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+        cv2.putText(outimg, text, (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
         
         fps = self.timer.stop()
         height = outimg.shape[0]
