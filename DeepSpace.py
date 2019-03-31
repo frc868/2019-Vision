eada7f985fcbc129667944065a41d885b936878f
Armaan Goel <armaangoel78@gmail.com>
2019-03-31 19:30:22 -0400

    added commenting and support for multiple vision setups

--- DeepSpace.py
+++ DeepSpace.py
@@ -21,6 +21,16 @@ import statistics as stat
 # @restrictions None
 # @ingroup modules
 
+# note that this is how the coordinate plane is layed out for the image
+# 0 ---------------- +x
+# |
+# | 
+# | 
+# | 
+# | 
+# |
+# +y
+
 class BoundingBox:
     def __init__(self, box):
         self.x = box[0]
@@ -28,23 +38,32 @@ class BoundingBox:
         self.w = box[2]
         self.h = box[3]
 
+    # returns box information (x,y coordinates, width and height)
     def getBox(self):
         return (self.x,self.y,self.w,self.h)
 
+    # draws the bounding box on the image
     def draw(self, img):
         cv2.rectangle(img,(self.x,self.y), \
                      (self.x+self.w,self.y+self.h),(0,255,0),2)
 
+    # returns distance between the two boxes
+    @staticmethod
     def distance(boxL, boxR):
         return boxR.x - boxL.x
 
+    # returns the position of the center of the two boxes
+    @staticmethod
     def position(boxL, boxR):
         return (boxL.x + BoundingBox.distance(boxL, boxR)/2)
 
+    # returns the ratio of the heights of the two boxes
+    @staticmethod
     def height_ratio(boxL, boxR):
         return boxL.h/boxR.h
 
-    # distance, position, h ratio
+    # returns distance, position, and h ratio
+    @staticmethod
     def calculate(boxL, boxR):
         return BoundingBox.distance(boxL, boxR), \
                BoundingBox.position(boxL, boxR), \
@@ -57,12 +76,16 @@ class FitLine:
         self.lx = line[2]
         self.ly = line[3]
     
+    # returns the slope of the line
     def slope(self):
         return float(self.vy/self.vx)
 
+    # returns the line information (x/y slope and x/y intersection)
     def getLine(self):
         return (self.vx,self.vy,self.lx,self.ly)
 
+    # returns the intersection point between two lines
+    @staticmethod
     def getIntersection(line0, line1):
         slope0 = line0.slope()
         slope1 = line1.slope()
@@ -75,6 +98,7 @@ class FitLine:
 
         return np.linalg.solve(a,b)
        
+    # draws lines on the image
     def draw(self, img):
         point0 = (self.lx - self.vx*100, self.ly - self.vy*100)
         point1 = (self.lx + self.vx*100, self.ly + self.vy*100)
@@ -87,10 +111,13 @@ class DetectedObject:
         self.box = BoundingBox(cv2.boundingRect(contour))
         self.line = FitLine(cv2.fitLine(contour, cv2.DIST_L2,0,0.01,0.01))
 
+    # draws bounding boxes and fitlines
     def draw(self, img):
         self.box.draw(img)
         self.line.draw(img)
 
+    # removes duplicate objects
+    @staticmethod
     def remove_dups(objs):
         def xPos(obj):
             return obj.box.x
@@ -100,68 +127,155 @@ class DetectedObject:
         while (i < len(objs) - 1):
             box0 = objs[i].box
             box1 = objs[i+1].box
+
+            # remove an object if the object is within the average width of two boxes in the x direction
             if (BoundingBox.distance(box0,box1) < (box0.w + box1.w)/2):
                 objs.remove(objs[i])
             i += 1
                 
         return objs
-        
+    
+    # returns true if the two objects given are a valid pair of vision targets (based on y-similarity and orientation)
+    @staticmethod
     def isValidPair(obj0, obj1):
         line0 = obj0.line
         line1 = obj1.line
         box0 = obj0.box
         box1 = obj1.box
         
-        def yPos(obj):
-            return obj.box.y
+        # is the y cordinate of the intersection of the two lines above the 
+        # TODO get this working again, as this is a better evaluation method
+        # cond1 = FitLine.getIntersection(line0, line1)[1] < (box0.y + box0.h + box1.y + box0.h)/2
 
+        # temporary fix to ensure that we don't select between two actual targets (ship)
+        # TODO remove when best solution (above) is implemented)
+        # ensures that the slope of line 0 is negative and the slope of line 1 is positive
+        # (please note that this is counterintuitive due to image coordinate plane being flipped, see coordinate plane above)
+        cond1 = (line0.slope() < 0) and (line1.slope() > 0)
+
+        # evaluate if the two targets are in the same general y-plane
         # the further away we get, the less pixels bounding boxes take up
         # so we need to scale it by distance between the two boxes
         box_dist = BoundingBox.distance(box0, box1)
         y_deadband = box_dist * 0.15 # tune as needed
-        
-        #jevois.sendSerial("Y-diff: " + str(yPos(obj0)-yPos(obj1)))
-
-        # intersection?
-        #jevois.sendSerial("thing 1: " + str(FitLine.getIntersection(line0, line1)[0]))
-        #jevois.sendSerial("thing 2: " + str((box0.y + box0.h + box1.y + box1.h)/ 2))
-        # cond1 = FitLine.getIntersection(line0, line1)[1] < (box0.y + box0.h + box1.y + box0.h)/2
-        cond1 = (line0.slope() < 0) and (line1.slope() > 0)
-
-        # jevois.sendSerial(str(FitLine.getIntersection(line0, line1)[1]) + " " + str((box0.y + box0.h + box1.y + box0.h)/2))
-        # same y plane?
-        cond2 = abs(yPos(obj0)-yPos(obj1)) < y_deadband
-        # cond2 = True
+        cond2 = abs(box0.y - box1.y) < y_deadband
 
         return (cond1 and cond2)
 
+# buffer of calculated values
 class ValueBuffer:
     def __init__(self, buffer_size):
         self.buffer_size = buffer_size
         self.buffer = []
 
+    # adds a value to the buffer
     def addValue(self, value):
+        # if the buffer isn't full, simply add the value
         if (len(self.buffer) < self.buffer_size):
             self.buffer.append(value)
+        # if the buffer is full, remove the first value, then add
         else:
             self.buffer = self.buffer[1:]
             self.buffer.append(value)
 
+    # returns average of the buffer
     def average(self):
         return np.average(self.buffer)
 
+    # returns the median of the buffer
     def median(self):
         return np.median(self.buffer)
     
+    # returns the standard deviation of the buffer
     def stdev(self):
         return np.std(self.buffer)
 
+    # returns the "derivative of the buffer" (really just the dy part from dy/dx)
     def deriv(self):
         rarr = []
         for i in range(0, self.buffer_size-1):
             rarr.append((self.buffer[i+1] - self.buffer[i]) / 2)
 
         return rarr
+
+class VisionSetup:
+    def __init__(self, hsvmin, hsvmax, rgbmin, rgbmax, kernel, erode_iter, dilate_iter):
+        self.hsvmin = hsvmin
+        self.hsvmax = hsvmax
+        self.rgbmin = rgbmin
+        self.rgbmax = rgbmax
+        self.kernel = kernel
+        self.erode_iter = erode_iter
+        self.dilate_iter = dilate_iter
+
+    # def hsvmin(self):
+    #     return self.hsvmin 
+
+    # def hsvmax(self):
+    #     return self.hsvmax
+
+    # def rgbmin(self):
+    #     return self.rgbmin
+
+    # def rgbmax(self):
+    #     return self.rgbmax
+
+    # def kernel(self):
+    #     return self.kernel
+
+    # def erode_iter(self):
+    #     return self.hsvmin
+
+    # def dilate_iter(self):
+    #     return self.dilate_iter
+
+    @staticmethod
+    def center_grove():
+        hsvmin = np.array([39,  211, 106])
+        hsvmax = np.array([100, 255, 255])
+        rgbmin = np.array([0, 0, 0])
+        rgbmax = np.array([255, 255, 255])
+        kernel = np.ones((2,2),np.uint8)
+        erode_iter = 2
+        dilate_iter = 5
+
+        return VisionSetup(hsvmin, hsvmax, rgbmin, rgbmax, kernel, erode_iter, dilate_iter)
+
+    @staticmethod
+    def devlins():
+        hsvmin = np.array([39,  211, 106])
+        hsvmax = np.array([100, 255, 255])
+        rgbmin = np.array([0, 0, 0])
+        rgbmax = np.array([255, 255, 255])
+        kernel = np.ones((2,2),np.uint8)
+        erode_iter = 2
+        dilate_iter = 5
+
+        return VisionSetup(hsvmin, hsvmax, rgbmin, rgbmax, kernel, erode_iter, dilate_iter)
+
+    @staticmethod
+    def gyansys():
+        hsvmin = np.array([39,  211, 106])
+        hsvmax = np.array([100, 255, 255])
+        rgbmin = np.array([0, 0, 0])
+        rgbmax = np.array([255, 255, 255])
+        kernel = np.ones((2,2),np.uint8)
+        erode_iter = 2
+        dilate_iter = 5
+
+        return VisionSetup(hsvmin, hsvmax, rgbmin, rgbmax, kernel, erode_iter, dilate_iter)
+
+    @staticmethod
+    def state():
+        hsvmin = np.array([39,  211, 106])
+        hsvmax = np.array([100, 255, 255])
+        rgbmin = np.array([0, 0, 0])
+        rgbmax = np.array([255, 255, 255])
+        kernel = np.ones((2,2),np.uint8)
+        erode_iter = 2
+        dilate_iter = 5
+
+        return VisionSetup(hsvmin, hsvmax, rgbmin, rgbmax, kernel, erode_iter, dilate_iter)
 
 class DeepSpace:
     def __init__(self):
@@ -172,31 +286,34 @@ class DeepSpace:
         self.posBuffer = ValueBuffer(buffer_size)
         self.hRatioBuffer = ValueBuffer(buffer_size)
 
+
     def run(self, inframe):
         text = "868V "
         data = ",,"
-        raw = inframe.getCvBGR()
+        raw = inframe.getCvBGR() # raw, unaltered image
+
+        setup = VisionSetup.center_grove()
         
         # filter by hsv values
         hsv = cv2.cvtColor(raw, cv2.COLOR_BGR2HSV)
-        hsvmin = np.array([39,  211, 106])
-        hsvmax = np.array([100, 255, 255])
+        hsvmin = setup.hsvmin
+        hsvmax = setup.hsvmax
         hsvfiltered = cv2.inRange(hsv, hsvmin, hsvmax)
 
         # filter by rgb values
         rgb = cv2.cvtColor(raw, cv2.COLOR_BGR2RGB)
-        rgbmin = np.array([0, 150, 0])
-        rgbmax = np.array([255, 255, 255])
+        rgbmin = setup.rgbmin
+        rgbmax = setup.rgbmax
         rgbfiltered = cv2.inRange(rgb, rgbmin, rgbmax)
 
         # combine both filtered version into one image
         filtered = cv2.bitwise_and(hsvfiltered, rgbfiltered)
         
         # erode, blur and dialate to remove noise
-        kernel = np.ones((2,2),np.uint8)
-        eroded = cv2.erode(hsvfiltered.copy(), kernel, iterations = 2)
+        kernel = setup.kernel
+        eroded = cv2.erode(filtered.copy(), kernel, iterations = setup.erode_iter)
         blurred = cv2.blur(eroded.copy(), (2, 2))
-        dilated = cv2.dilate(blurred.copy(), kernel, iterations = 5)
+        dilated = cv2.dilate(blurred.copy(), kernel, iterations = setup.dilate_iter)
 
 
         # detect edges
@@ -210,7 +327,7 @@ class DeepSpace:
         editimg = raw.copy()
 
         if (cnts is not None) and (len(cnts) > 0):
-            # sorts contours by area (largest to smallest) and gets top ...eight...
+            # sorts contours by area (largest to smallest) and gets top 4
             cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:4]
 
             # create a detected object for each contour
@@ -265,7 +382,7 @@ class DeepSpace:
                 d = stat.stdev(self.distBuffer.deriv())   < 1
                 p = stat.stdev(self.posBuffer.deriv())    < 1
                 h = stat.stdev(self.hRatioBuffer.deriv()) < 1
-                # 2/3 correct
+                # 2/3 conditions must be met
                 if (h and (d ^ p)) or (d and p): 
                   text = text + "Dist: " + str(dist) + " Pos: " + str(pos) \
                          + " H_Ratio: " + str(h_ratio)
